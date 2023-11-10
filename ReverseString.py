51653614dba8f36739a7acd934c24051d72616e9
TheCodingCat-git <thecodingcat7@gmail.com>
2023-11-11 00:20:40 +0530

    Create ReverseString.py

---
+++ ReverseString.py
@@ -0,0 +1,11 @@
+# Question - Write a Python program that accepts a word from the user and reverse it.
+""" Expected Output - 
+        Input : Edyoda
+
+        output: adoydE """
+
+str = "Edyoda"
+
+# Print Reversed String using for-loop
+for char in reversed(str):
+    print(char, end="")
